29e8e06d9bb72e22ae6abbb81d1035a90e0567ac
Shelby Lee <slee337@uw.edu>
2022-11-01 10:51:59 -0600

    client side routing business

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,10 +11,20 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
-      <h1> all these dumb hoes wanting to rider stupid bikes and sleep in ignorant dirt </h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/testing">
+            <h1>Test Route</h1>
+            <h1>maui wowie pineapple express lets ride</h1>
+          </Route>
+          <Route path="/">
+            <h1>Page Count: {count}</h1>
+            <h1> all these dumb hoes wanting to rider stupid bikes and sleep in ignorant dirt </h1>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # route to test your configuration
-  get '/hello', to: 'application#hello_world'
-
-
-end
+  Rails.application.routes.draw do
+    get '/hello', to: 'application#hello_world'
+  
+    get '*path',
+        to: 'fallback#index',
+        constraints: ->(req) { !req.xhr? && req.format.html? }
+  end
